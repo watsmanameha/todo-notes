9c6a772f3b5db7aef5446ee9d64171259584683a
Eugene <evgeshka120@gmail.com>
2022-02-28 18:53:30 +0300

    Changed User model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-    username = models.CharField(max_length=64)
+class User(AbstractUser):
+    username = models.CharField(max_length=64, unique=True)
     firstname = models.CharField(max_length=64)
     lastname = models.CharField(max_length=64)
-    email = models.CharField(max_length=64, unique=True)
+    email = models.EmailField(unique=True)
