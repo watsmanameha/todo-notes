1ebbbbbae45455724021c3e54f578397fdb5a121
Eugene <evgeshka120@gmail.com>
2022-03-26 09:32:14 -0700

    Created Todo model

--- todo/models.py
+++ todo/models.py
@@ -7,3 +7,11 @@ class Project(models.Model):
     repo_url = models.URLField(max_length=200)
     users = models.ManyToManyField(User)
 
+
+class Todo(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField()
