b4039572e89d3d983faf7c733ceee9ec140987ef
Eugene <evgeshka120@gmail.com>
2022-03-26 09:18:48 -0700

    Created Project model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=64)
+    repo_url = models.URLField(max_length=200)
+    users = models.ManyToManyField(User)
+
