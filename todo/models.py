9876c3097e056118c7ac2450118563436dcf2736
Eugene <evgeshka120@gmail.com>
2022-03-31 00:13:53 +0300

    Changed Todo API

--- todo/models.py
+++ todo/models.py
@@ -8,11 +8,14 @@ class Project(models.Model):
     repo_url = models.URLField(max_length=200)
     users = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.project_name
+
 
 class Todo(models.Model):
-    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    project = models.OneToOneField(Project, on_delete=models.CASCADE, unique=True)
     text = models.TextField()
     created_at = models.DateField(auto_now=False, auto_now_add=False)
     updated_at = models.DateField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     is_active = models.BooleanField()
--- todonotes/settings.py
+++ todonotes/settings.py
@@ -145,5 +145,6 @@ REST_FRAMEWORK = {
         "djangorestframework_camel_case.parser.CamelCaseJSONParser",
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
