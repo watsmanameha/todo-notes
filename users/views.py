c3a2656385fb931df86acd1a727b5c00f660d67d
Eugene <evgeshka120@gmail.com>
2022-03-30 23:34:10 +0300

    Changed UserModel API

--- users/views.py
+++ users/views.py
@@ -1,9 +1,19 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserModelSerializer
+class UserModelViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        users = User.objects.all()
+        serializer = UserModelSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserModelSerializer
+        return Response(serializer.data)
