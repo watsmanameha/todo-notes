da30dffbf40a7e5f17bb62a26b41099bd1abb16b
Eugene <evgeshka120@gmail.com>
2022-04-11 09:25:01 +0300

    Rebuild

--- todonotes/urls.py
+++ todonotes/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
 
 from todo.views import ProjectModelViewSet, TodoModelViewSet
 from users.views import UserModelViewSet
@@ -14,6 +14,8 @@ router.register("todos", TodoModelViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path('api-token-auth/', views.obtain_auth_token),
     path("api/", include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
