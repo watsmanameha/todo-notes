0652359d825c1712900a08f8b12e1425c2711090
Eugene <62775429+watsmanameha@users.noreply.github.com>
2022-04-11 09:34:16 +0300

    Revert "Lesson 6 (#7)"
    This reverts commit 157d43aa957038813608e7151387cf58ead34a5c.

--- todonotes/urls.py
+++ todonotes/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
+from rest_framework.authtoken import views
 
 from todo.views import ProjectModelViewSet, TodoModelViewSet
 from users.views import UserModelViewSet
@@ -16,7 +16,4 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path('api-token-auth/', views.obtain_auth_token),
     path("api/", include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
