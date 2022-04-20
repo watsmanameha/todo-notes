c0f4f2dab2fb009caafec72977c55b97486db12d
Eugene <62775429+watsmanameha@users.noreply.github.com>
2022-04-21 01:34:42 +0400

    created tests (#10)

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,18 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
 from .models import User
 
 
-class UserModelSerializer(HyperlinkedModelSerializer):
+class UserModelSerializer(ModelSerializer):
+    # id = PrimaryKeyRelatedField(
+    #     queryset=User.objects.all(),
+    #     allow_null=True,
+    #     default=None
+    # )
+
+    # def validate_user(self, value):
+    #     return self.context['request'].user
+
     class Meta:
         model = User
-        fields = ["username", "firstname", "lastname", "email"]
+        fields = ["id", "username", "firstname", "lastname", "email"]
