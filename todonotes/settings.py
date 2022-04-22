2ce0b91d1f4b1de8b9f33deb524c9ddc413960c2
Eugene <evgeshka120@gmail.com>
2022-04-22 17:27:55 +0400

    api documentation

--- todonotes/settings.py
+++ todonotes/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework.authtoken",
     "rest_framework_simplejwt.token_blacklist",
+    'drf_yasg',
     "users",
     "todo",
 ]
--- todonotes/urls.py
+++ todonotes/urls.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from todo.views import TodoViewSet, ProjectViewSet
 from users.views import UserViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todo Notes",
+        default_version='0.1',
+        description="'Todo Notes' documentation",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
@@ -25,4 +40,12 @@ urlpatterns = [
 
     path('api/users/0.1', include('users.urls', namespace='0.1')),
     path('api/users/0.2', include('users.urls', namespace='0.2')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
+
